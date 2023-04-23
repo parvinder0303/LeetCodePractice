3b116ae0e33983492ac7455c22daf8e4df3ed8be
parvinder0303 <85359235+parvinder0303@users.noreply.github.com>
2023-04-23 16:53:34 -0500

    Update SortedSquaresArray.java

--- src/ArrayPractice/SortedSquaresArray.java
+++ src/ArrayPractice/SortedSquaresArray.java
@@ -18,7 +18,7 @@ public class SortedSquaresArray {
         for(int i=0; i<ar.length;i++){
             System.out.print(ar[i]+" ");
         }
-       /* for (int i = 1; i<ar.length; i++) {
+        for (int i = 1; i<ar.length; i++) {
             for(int j=0;j<ar.length-1;j++){
              k=ar[j];
                 if(k>ar[j+1]){
@@ -33,6 +33,6 @@ public class SortedSquaresArray {
         }
         for(int i=0;i<ar.length;i++){
             System.out.println(ar[i]);
-        }*/
+        }
     }
 }
